0c10a66bbddc75109ede5089cb9699ad67a9dc19
nick <nickpturner98@gmail.com>
2018-11-22 04:44:09 +0000

    forgot to add another @author tag(I need to pay more attention to this stuff)

--- src/InventoryCommand.java
+++ src/InventoryCommand.java
@@ -1,13 +1,15 @@
-/**this command lists all the items in the adventurer's inventory
- */
+
 import java.util.ArrayList;
 
+/**this command lists all the items in the adventurer's inventory
+@author Alexander Loveland
+ */
 class InventoryCommand extends Command {
 
     InventoryCommand() {
     }
 /** This method will return a string that lists all the itmes in the adventurues inventory by
- * cycling through all the items in the 
+ * cycling through all the items in the
  * adventurues inventory in the order they appear in the ArrayList created within this method
  */
     public String execute() {
