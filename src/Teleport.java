94c591db96c86601b124090d5d288f690eabab20
Benjamin Madren <bmadren@mail.umw.edu>
2018-11-22 16:22:46 +0000

    Added Transform and Teleport classes.

---
+++ src/Teleport.java
@@ -0,0 +1,15 @@
+/** @author Benjamin Madren 
+ * When triggered, this class will randomly teleport the user 
+ * to some other room in the dungeon, which they may or may not 
+ * have previously visited. 
+ * This class will interact with the GameState class. **/
+	public class Teleport{ 
+		/**This method will set the user's current room 
+		 * to their destination. 
+		 * @param destination **/ 
+		GameState setAdventurersCurrentRoom(){ 
+		
+		} 
+		/**The class reutrns the user's destination. 
+		 * @return destination **/
+	}
---
+++ src/Transform.java
@@ -0,0 +1,19 @@
+/** @author Benjamin Madren 
+  * When this class is triggered, the corresponding item will disappear 
+  * and be replaced by another item whose name is in parentheses. 
+  * This item will not have existed previously. 
+  * This class also interacts with the GameState class. **/
+	public class Transform{ 
+	/**This method removes the selected item from the user's inventory. 
+	 * @param Item1 **/ 
+	Item removeFromInventory(){ 
+	
+	} 
+	/**This method adds a new item to the user's inventory. 
+	 * @param Item2 **/ 
+	Item addToInventory(){ 
+	
+	} 
+	/**The class returns the new item. 
+	 * @return Item2 **/
+	}
