0200dd9dede2e93217ef52f814653b7295983e5a
Benjamin Madren <bmadren@mail.umw.edu>
2018-12-09 20:19:25 +0000

    Fixed light event.

--- src/Light.java
+++ src/Light.java
@@ -2,7 +2,7 @@
  * There are many dark rooms throughout the dungeon. The player needs a
  * light source to progress through these rooms.
  * @author Benjamin Madren **/
-public class Light {
+abstract class Light extends Command{
   public Item light;
   public boolean power = true;
   public boolean dark = true;
