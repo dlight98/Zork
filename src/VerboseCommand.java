16d0f9789a6d90309da9467e1973fa7c402aa81a
nick <nickpturner98@gmail.com>
2018-12-10 16:39:00 +0000

    got verbose working with lowercase

--- src/VerboseCommand.java
+++ src/VerboseCommand.java
@@ -1,22 +1,22 @@
 
 class VerboseCommand extends Command
-	{
-		private boolean value;
-	public VerboseCommand(boolean value)
-	{
-		this.value = value;
-	}
+{
+  private boolean value;
+  public VerboseCommand(boolean value)
+  {
+    this.value = value;
+  }
 
 
-	public String execute(){
-		if(value == true)
-		{
-			GameState.instance().getDungeon().VerboseExecute(true);
-			return "Verbose is now on, all rooms will display their descriptions";
-		}
-		else{
-			GameState.instance().getDungeon().VerboseExecute(false);
-       			return "Verbose is now off, rooms will not display their descriptionss";
-	}
-	}
-	}	
+  public String execute(){
+    if(value == true)
+    {
+      GameState.instance().getDungeon().VerboseExecute(true);
+      return "Verbose is now on, all rooms will display their descriptions.\n";
+    }
+    else{
+      GameState.instance().getDungeon().VerboseExecute(false);
+      return "Verbose is now off, rooms will not display their descriptionss.\n";
+    }
+  }
+}	
